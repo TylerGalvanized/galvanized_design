7a07ea4a54b33709438ce1ac64537b77196790d1
Tyler Galvanized <tyler.bankston@galvanizeddesign.com>
2016-08-25 13:31:28 -0600

    app init commit

---
+++ GalvanizedDesign/GalvanizedDesign/scripts/galvanized.config.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+(function () {
+  angular
+    .module('GalvanizedDesign')
+    .config([
+      '$mdThemingProvider',
+      GalvanizedDesignConfig
+    ]);
+
+  function GalvanizedDesignConfig($mdThemingProvider) {
+
+    $mdThemingProvider
+      .theme('default')
+      .primaryPalette('grey')
+      .accentPalette('yellow')
+      .backgroundPalette('grey');
+  };
+
+}());
---
+++ GalvanizedDesign/GalvanizedDesign/scripts/galvanized.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+(function () {
+  angular.module('GalvanizedDesign', [
+    'ngMaterial',
+    'ngAnimate',
+    'ngRoute'
+  ]);
+}());
